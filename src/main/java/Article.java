309f76b235749bf7b6809c5844861a60d6906f0a
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2025-11-13 12:21:34 +0100

    getter für Article klasse

--- src/main/java/Article.java
+++ src/main/java/Article.java
@@ -1,2 +1,24 @@
-﻿public abstract class Article {
+﻿import java.time.LocalDateTime;
+
+public abstract class Article {
+    String author;
+    String title;
+    LocalDateTime publicationTime;
+    NewsCategory category;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getPublicationTime() {
+        return publicationTime;
+    }
+
+    public NewsCategory getCategory() {
+        return category;
+    }
 }
