a932bfb0a68439c66da3b059341aa5ec00d0af5e
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2025-11-13 12:18:25 +0100

    NewsException klasse implementiert

--- src/main/java/NewsException.java
+++ src/main/java/NewsException.java
@@ -1,2 +1,8 @@
-﻿public class NewsException {
+﻿public class NewsException extends RuntimeException {
+    public NewsException(String message) {
+        super(message);
+    }
+    public NewsException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
